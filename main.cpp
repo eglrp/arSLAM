2dcf3839b319fba960a35e20b5b7931f8e15ddd5
steve <551619855@qq.com>
2017-01-12 19:31:01 +0800

    error...

--- main.cpp
+++ main.cpp
@@ -29,15 +29,18 @@ int main() {
     cv::Ptr<cv::aruco::Dictionary> dic_ptr(cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_100));
 
 
-    cv::Mat3d intrinsic_matrix;
-    cv::Mat distortion_matrix;
-    distortion_matrix.resize(5);
+    cv::Mat intrinsic_matrix(3,3,CV_32F);
+    cv::Mat distortion_matrix(1,5,CV_32F);
+//    distortion_matrix.resize(5);
 
     std::fstream tfs("./data/distortion_matrix.txt");
     for (int k(0); k < 5; ++k) {
         double ttt;
         tfs >> ttt;
-        distortion_matrix.push_back(ttt);
+//        distortion_matrix.push_back(ttt);
+        distortion_matrix.at<double>(0,k) = ttt;
+//        std::cout << distortion_matrix << std::endl;
+
     }
 
 //
@@ -48,9 +51,12 @@ int main() {
         for (int j(0); j < 3; ++j) {
             double d_tmp(0.0);
             ifs >> d_tmp;
-            intrinsic_matrix.push_back(d_tmp);
+//            intrinsic_matrix.push_back(d_tmp);
+            intrinsic_matrix.at<double>(ii,j) = d_tmp;
         }
     }
+
+
 
 //    for (int x(0); x < intrinsic_matrix.rows; ++x) {
 //        for (int y(0); y < intrinsic_matrix.cols; ++y) {
@@ -70,9 +76,10 @@ int main() {
 
         cap >> in_img;
         out_img_ptr = &in_img;
-        std::cout <<"here" << std::endl;
         std::vector<std::vector<cv::Point2f>> corner;
         std::vector<int> ids;
+
+
         cv::aruco::detectMarkers(in_img, dic_ptr, corner, ids);
 //        cv::aruco::drawAxis(out_img,)
 
@@ -83,20 +90,30 @@ int main() {
                 cv::aruco::drawDetectedMarkers(in_img, corner, ids);
             }
 
-            std::cout << "eee" << std::endl;
-            std::vector<cv::Vec3f> rvecs, tvecs;
-            cv::aruco::estimatePoseSingleMarkers(corner,
-                                                 201,
-                                                 intrinsic_matrix,
-                                                 distortion_matrix,
-                                                 rvecs,
-                                                 tvecs);
-            for (int i(0); i < corner.size(); ++i) {
+            std::vector<cv::Vec3d> rvecs, tvecs;
+//            std::cout << 1.1<<std::endl;
+            try{
+                cv::aruco::estimatePoseSingleMarkers(corner,
+                                                     201,
+                                                     intrinsic_matrix,
+                                                     distortion_matrix,
+                                                     rvecs,
+                                                     tvecs);
+            }catch(cv::Exception a)
+            {
+                std::cout << "error in estimate pose:"<<
+                                                      a.err << std::endl;
+            }
+
+//            std::cout << 1.2 << std::endl;
+            for (int i(0); i < rvecs.size(); ++i) {
                 //201 mm
 
 
+                std::cout << rvecs[i] << "   " << tvecs[i] << std::endl;
+
                 cv::aruco::drawAxis(in_img,intrinsic_matrix,distortion_matrix,
-                rvecs[i],tvecs[i],0.1);
+                rvecs[i],tvecs[i],100.1);
             }
 
 //            cv::aruco::drawAxis(in_img, intrinsic_matrix, distortion_matrix,)
