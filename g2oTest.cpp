651158dec9941ece7b58b5fb0640a5a0bfaa0a39
steve <551619855@qq.com>
2017-01-24 23:26:50 +0800

    error in use g2o...

--- g2oTest.cpp
+++ g2oTest.cpp
@@ -37,8 +37,8 @@
 #include "g2o/solvers/csparse/linear_solver_csparse.h"
 
 #include "g2o/core/factory.h"
-//#include "g2o/types/slam3d/types_slam3d.h"
-//#include "g2o/types/slam2d/types_slam2d.h"
+#include "g2o/types/slam3d/types_slam3d.h"
+#include "g2o/types/slam2d/types_slam2d.h"
 
 #include "g2o/stuff/command_args.h"
 
