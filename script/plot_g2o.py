73c8a2c3f4ee3d9f74a6eab5a3f48c3ae4542d07
steve <551619855@qq.com>
2017-02-15 14:20:46 +0800

    g2oResult show.

---
+++ script/plot_g2o.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding=utf-8
+# created by steve 17-2-15 下午1:51
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
+
+from array import array
+
+if __name__ == '__main__':
+    g2o_file = open("../after_graph.g2o")
+
+    odo_list = array("d")
+    markers_list = array("d")
+
+    for line in g2o_file:
+        if 'VERTEX_SE3' in line:
+            id = line.split(' ')[1]
+            id = float(id)
+            if id > 1000:
+                odo_list.append(float(line.split(' ')[2]))
+                odo_list.append(float(line.split(' ')[3]))
+                odo_list.append(float(line.split(' ')[4]))
+            if id > 0 and id < 1000:
+                markers_list.append(float(line.split(' ')[2]))
+                markers_list.append(float(line.split(' ')[3]))
+                markers_list.append(float(line.split(' ')[4]))
+
+    odo = np.frombuffer(odo_list, dtype=np.float).reshape(-1, 3)
+    marker = np.frombuffer(markers_list, dtype=np.float).reshape(-1, 3)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111,projection='3d')
+    ax.plot(odo[:,0],odo[:,1],odo[:,2],'r-+')
+    ax.plot(marker[:,0],marker[:,1],marker[:,2],'b*')
+
+
+    plt.figure(3)
+    plt.plot(odo[:, 0], odo[:, 1], 'r*-')
+    plt.plot(marker[:, 0], marker[:, 1], 'b+')
+    plt.grid(True)
+    plt.show()
