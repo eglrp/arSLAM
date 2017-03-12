7a0067e8155f9d5019d23ffd5d2eca4cfdb0c511
steve <551619855@qq.com>
2017-03-12 14:49:45 +0800

    generator real pose for each imudata and uwbdata.

---
+++ script/PairsRealPose.py
@@ -0,0 +1,8 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    time_pose = np.loadtxt("time2pose.csv",delimiter=',')
+
+    print(time_pose)
